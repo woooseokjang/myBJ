b2428bfe574c968bcabbbbb1656db594c452d879
Wooseok Jang <mmyu2090@gmail.com>
2022-05-26 12:12:56 +0900

    FIX: 1157 fixed

--- 1157.py
+++ 1157.py
@@ -5,7 +5,16 @@ for c in s:
     arr[ord(c.upper()) - 65] = arr[ord(c.upper()) - 65] + 1
 max = max(arr)
 index = arr.index(max)
-if len(list(filter(lambda i: arr[i] == max, range(len(arr))))) >= 2:
-    print('?')
-else:
-    print(chr(index + 65))
+cnt = 0
+for i in arr:
+    if i == max:
+        cnt = cnt + 1
+    if cnt > 1:
+        print('?')
+        exit()
+print(chr(index + 65))
+
+# if len(list(filter(lambda i: arr[i] == max, range(len(arr))))) >= 2:
+#    print('?')
+# else:
+#    print(chr(index + 65))
