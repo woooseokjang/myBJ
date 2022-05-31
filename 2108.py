b1227b4c3c9e7c95f9dad63447c551c4a8a0507b
Wooseok Jang <mmyu2090@gmail.com>
2022-06-01 01:08:27 +0900

    FIX: 2108.py is not functional yet

--- 2108.py
+++ 2108.py
@@ -1,35 +1,31 @@
+from collections import Counter
+
 n = int(input())
-arr = [0 for _ in range(8001)]
+arr = [None for _ in range(8001)]
 arr2 = []
-arr3 = []
 sum = 0
 for _ in range(n):
     i = int(input())
     sum = sum + i
     arr2.append(i)
     i = i + 4000
+    if arr[i] == None:
+        arr[i] = 0
     arr[i] = arr[i] + 1
 
 
-print("{:.0f}".format(sum / n))
+print(round(sum / n))
 arr2.sort()
 print(arr2[len(arr2)//2])
-
-max = 0
-index = -1
-
-for i in range(8001):
-    if max == 0 and arr[i] > max:
-        max = arr[i]
-        index = i
-    elif max != 0 and arr[i] > max:
-        arr3.append([max, index])
-        max = arr[i]
-        index = i
-    elif max != 0 and arr[i] == max:
-
-max = arr.index(max(arr))
-print(max)
-
-
+cnt = Counter(arr)
+most = cnt.most_common()
+if most[0][0] == None:
+    most = most[1:]
+most.sort(key=lambda x: x[1])
+most.reverse()
+if most[0][1] == 1:
+    print(arr.index(most[0][0]) - 4000)
+else:
+    arr3 = list(filter(lambda x: arr[x] == most[0][0], range(len(arr))))
+    print(arr3[1] - 4000)
 print(arr2[len(arr2) - 1] - arr2[0])
