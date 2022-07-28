2ccf07b505dcbe1c3faa7f52dd41594cfd54e61e
Wooseok Jang <mmyu2090@gmail.com>
2022-07-28 17:13:53 +0900

    fix: 2108 fixed
    Signed-off-by: Wooseok Jang <mmyu2090@gmail.com>

--- 2108.py
+++ 2108.py
@@ -1,31 +1,21 @@
 from collections import Counter
+import sys
 
-n = int(input())
-arr = [None for _ in range(8001)]
-arr2 = []
-sum = 0
-for _ in range(n):
-    i = int(input())
-    sum = sum + i
-    arr2.append(i)
-    i = i + 4000
-    if arr[i] == None:
-        arr[i] = 0
-    arr[i] = arr[i] + 1
+n = int(sys.stdin.readline())
+arr = []
 
+for i in range(n):
+    arr.append(int(sys.stdin.readline()))
 
-print(round(sum / n))
-arr2.sort()
-print(arr2[len(arr2)//2])
-cnt = Counter(arr)
-most = cnt.most_common()
-if most[0][0] == None:
-    most = most[1:]
-most.sort(key=lambda x: x[1])
-most.reverse()
-if most[0][1] == 1:
-    print(arr.index(most[0][0]) - 4000)
+print(round(sum(arr) / n))
+arr.sort()
+print(arr[int(i / 2)])
+counter = Counter(arr).most_common()
+if len(counter) > 1:
+    if counter[0][1] == counter[1][1]:
+        print(counter[1][0])
+    else:
+        print(counter[0][0])
 else:
-    arr3 = list(filter(lambda x: arr[x] == most[0][0], range(len(arr))))
-    print(arr3[1] - 4000)
-print(arr2[len(arr2) - 1] - arr2[0])
+    print(counter[0][0])
+print(arr[-1] - arr[0])
