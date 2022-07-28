689b2a592960d8adc3f9b82ea9a5b373fd31efec
Wooseok Jang <mmyu2090@gmail.com>
2022-07-28 22:26:54 +0900

    Add: 2805

---
+++ 2805.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+tree = list(map(int, input().split()))
+start, end = 1, max(tree)
+
+while start <= end:
+    mid = (start + end) // 2
+    log = 0
+    for i in tree:
+        if i > mid:
+            log += i - mid
+
+    if log >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+
+print(end)
