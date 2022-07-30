7465230aba71293084a923c30dcc79c5786a129b
Wooseok Jang <mmyu2090@gmail.com>
2022-07-31 01:28:32 +0900

    ADD: 1260

---
+++ 1260.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(len(graph)):
+    graph[i].sort()
+
+
+def dfs(start):
+    print(start, end=' ')
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i)
+            visited[i] = True
+
+
+visited = [False] * (n+1)
+dfs(v)
+print()
+
+
+def bfs(start):
+    q = deque([start])
+    visited[start] = True
+    while q:
+        now = q.popleft()
+        print(now, end=' ')
+        for i in graph[now]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = True
+
+
+visited = [False] * (n + 1)
+bfs(v)
