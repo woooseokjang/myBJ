b6008254763ab8c3b0d2a0bb9e3117ac0912cb2c
Wooseok Jang <mmyu2090@gmail.com>
2022-07-31 00:38:28 +0900

    FIX: 1074

--- 1074.py
+++ 1074.py
@@ -1,2 +1,25 @@
 # https://www.acmicpc.net/problem/1074
-n = int(input())
+
+n, r, c = map(int, input().split())
+
+ans = 0
+
+while n != 0:
+
+    n -= 1
+    border = pow(2, n)
+
+    if r < border and c < border:
+        None
+    elif r < border and c >= border:
+        c -= border
+        ans += border * border
+    elif r >= border and c < border:
+        r -= border
+        ans += border * border * 2
+    elif r >= border and c >= border:
+        c -= border
+        r -= border
+        ans += border * border * 3
+
+print(ans)
